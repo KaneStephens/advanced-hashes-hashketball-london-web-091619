63bdd3cf7e17e5deb6ae996cc97e366ff5667225
Kane Pangalia Stephens <kanepstephens@gmail.com>
2019-09-04 16:53:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -116,7 +116,7 @@ def num_points_scored(name_of_player)
   game_hash.each_key {|team|
     search_spot = game_hash[team][:players]
     
-    search_spot.each { |player_hash|
+    search_spot.map { |player_hash|
       the_key = search_spot[player_hash]
       print(the_key)
       
