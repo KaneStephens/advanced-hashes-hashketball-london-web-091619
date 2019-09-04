487fe50aa9be288a782f3e01381edefbd7abe1c5
Kane Pangalia Stephens <kanepstephens@gmail.com>
2019-09-04 19:24:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -173,7 +173,7 @@ def player_stats(player_name)
     game_hash.each { |team, info|
     info[:players].each { |player|
       if player[:player_name] == player_name then
-        return  player.select {|keys, values| keys != player_name} 
+        return  player.select! {|keys, values| keys != player_name} 
       end
     }
   }
